b8bd981c97e3afbbb58ccdd09ad4aee117024629
davinod <davinod@gmail.com>
2015-10-15 01:39:34 -0300

    Added views and layouts using handlebars. This is awesome!

--- NodeJS/meadowlark/meadowlark.js
+++ NodeJS/meadowlark/meadowlark.js
@@ -2,21 +2,38 @@ var express = require('express');
 
 var app = express();
 
+var handlebars = require('express-handlebars')
+	.create({ defaultLayout:'main' });
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
+
+// ******** Routes *********
+
+// Home
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+// About
+app.get('/about', function(req, res){
+	res.render('about');
+});
+
+// ******* Exceptions ********
 
 // custom 404 page
 app.use(function(req, res){
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not Fond');
+	res.render('404');
 });
 
 // custom 500 page
 app.use(function(req, res){
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
 
 app.listen(app.get('port'), function(){
