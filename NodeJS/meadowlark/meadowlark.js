64b1c6d1aea8736f9ed164bf5d5262b85668fcb1
davinod <davinod@gmail.com>
2015-11-01 20:32:36 -0200

    removed fileupload via jquery added cookie usage

--- NodeJS/meadowlark/meadowlark.js
+++ NodeJS/meadowlark/meadowlark.js
@@ -4,6 +4,9 @@ var app = express();
 var handlebars = require('express-handlebars').create({
 	defaultLayout:'main' });
 var fortune = require('./lib/fortune.js');
+var formidable = require('formidable');
+var jqupload = require('jquery-file-upload-middleware');
+var credentials = require('./credentials.js');
 
 function getWeatherData(){
 	return{
@@ -53,6 +56,22 @@ app.use(function(req, res, next){
 //newsletter
 app.use(require('body-parser').urlencoded({ extended:  true }));
 
+//Cookies
+app.use(require('cookie-parser')(credentials.cookieSecret));
+
+//Upload
+app.use('/upload', function(req, res, next){
+	var now = Date.now();
+	jqupload.fileHandler({
+		uploadDir: function(){
+			return __dirname + '/public/uploads/' + now;
+		},
+		uploadUrl: function(){
+			return '/uploads/' + now;
+		},
+	})(req, res, next);
+});
+
 // ******** Routes *********
 
 app.use(function(req, res, next){
@@ -66,17 +85,45 @@ app.get('/', function(req, res){
 	res.render('home');
 });
 
+//vacation-photo
+app.get('/contest/vacation-photo', function(req, res){
+	var now = new Date();
+	res.cookie('monster', 'nom nom');
+	res.cookie('signed_monster', 'nom nom2', { signed: true });
+	res.render('contest/vacation-photo',{
+		year: now.getFullYear(),month: now.getMonth()
+	});
+});
+
+//vacation-photo submission
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+	var form = new formidable.IncomingForm();
+	var monster = req.cookies.monster;
+	var signedMonster = req.signedCookies.signed_monster;
+	console.log('monster=' + monster);
+	console.log('signedMonster=' + signedMonster);
+	form.parse(req, function(err, fields, files){
+		if(err) return res.redirect(303, '/error');
+		console.log('received fields:');
+		console.log(fields);
+		console.log('name=' + fields.name);
+		console.log('email=' + fields.email);
+		console.log('received files:');
+		console.log(files);
+		
+		res.redirect(303, '/thank-you');
+	});
+});
+
 //Newsletter
 app.get('/newsletter', function(req, res){
 	res.render('newsletter', { csrf: 'CSRF token goes here' });
 });
 
 //Newsletter thank-you
-//app.get('/thank-you', function(req, res){
-//	console.log('req.body.name: ' + req.query.name);
-//	console.log('req.body.email: ' + req.query.email);
-//	res.render('thank-you', { name: req.query.name, email: req.query.email });
-//});
+app.get('/thank-you', function(req, res){
+	res.render('thank-you');
+});
 
 app.post('/process', function(req, res){
 	if(req.xhr || req.accepts('json,html')==='json'){
